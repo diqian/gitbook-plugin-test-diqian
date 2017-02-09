5227b4634a920e86342a7450bcf37cbf1ad290c6
diqian <qiandi.eric@gmail.com>
2017-02-09 09:29:48 -0800

    Tryout

---
+++ README.md
@@ -0,0 +1,20 @@
+## plugin-diqian
+
+To support my own custom font family and font embed in gitbook web version.
+
+
+### To Install
+
+```
+npm install .. to be filled
+```
+
+In `book.json` ,
+
+add `to be filled` in `plugins`
+
+```
+"plugins": [
+      "plugin-to be filled"
+      ]
+```
---
+++ index.js
@@ -0,0 +1,20 @@
+module.exports = {
+
+    book: {
+        assets: "./book",
+        js: [],
+        css: ["diqian.css"]
+    }
+    // Map of hooks
+    hooks: {},
+
+    // Map of new blocks
+    blocks: {},
+
+    // Map of new filters
+    filters: {},
+};
+
+exports.printMsg = function() {
+    console.log("This message is from the npm demo"); 
+};
